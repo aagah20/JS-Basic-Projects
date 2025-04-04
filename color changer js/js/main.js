4e4113018a9cb7ee5687919028a2997a5dddcfd9
aagah20 <gahaachal@gmail.com>
2025-04-04 20:01:36 +0530

    Add files via upload

---
+++ color changer js/js/main.js
@@ -0,0 +1,21 @@
+document.querySelector(".btn").addEventListener("click", changeColor);
+
+function changeColor(){
+    let colors = ["Lightblue","Pink","Purple","Violet","Lightgreen","Lavender","BurlyWood","DarkSalmon","GreenYellow","LightCoral","LightGray","LightPink","MistyRose","Turquoise","Teal"];
+    let randomColor = Math.floor(Math.random()* colors.length);
+    console.log(randomColor);
+
+    document.querySelector("span").innerText = colors[randomColor];
+    document.querySelector("body").style.backgroundColor = colors[randomColor]
+
+    document.querySelector("h2").style.backgroundColor = "black";
+    document.querySelector("h2").style.color = "white"
+    document.querySelector("h2").style.padding = "0.7em"
+
+} 
+   
+
+
+
+
+
