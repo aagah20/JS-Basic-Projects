4874b69e0d6febe77cd49425f8549b08590daa63
aagah20 <gahaachal@gmail.com>
2025-04-04 20:59:53 +0530

    Update main.js

--- color changer js/js/main.js
+++ color changer js/js/main.js
@@ -7,7 +7,7 @@ function changeColor(){
 
     document.querySelector("span").innerText = colors[randomColor];
     document.querySelector("body").style.backgroundColor = colors[randomColor]
-
+    document.querySelector(".btn").style.backgroundColor = colors[randomColor]
     document.querySelector("h2").style.backgroundColor = "black";
     document.querySelector("h2").style.color = "white"
     document.querySelector("h2").style.padding = "0.7em"
